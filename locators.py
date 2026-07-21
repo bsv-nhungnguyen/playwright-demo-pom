8201a58b23e1f02fadaef0dc33078cf9431f0598
Nhung (VS Code) <nhungnguyen@bravesoft.com.vn>
2026-07-21 14:22:48 +0700

    feat: demo_002

--- locators.py
+++ locators.py
@@ -2,4 +2,4 @@
 class LoginPageLocators:
     INPUT_USERNAME = "//input[@placeholder = 'メールアドレス']" #メールアドレス
     INPUT_PASSWORD = "//input[@placeholder = 'パスワード']" #パスワード
-    BUTTON_LOGIN = "//button[@type='submit']" #ログイン
+    BUTTON_LOGIN = "//button[@type='submit']" #ログイン (Login)
