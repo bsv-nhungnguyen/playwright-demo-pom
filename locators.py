d8184e091586349c18894cd418ff614a20867479
Nhung (Cursor) <nhung14042001@gmail.com>
2026-07-21 14:39:47 +0700

    create conflict 2B

--- locators.py
+++ locators.py
@@ -3,3 +3,9 @@ class LoginPageLocators:
     INPUT_USERNAME = "//input[@placeholder = 'メールアドレス']" #メールアドレス
     INPUT_PASSWORD = "//input[@placeholder = 'パスワード']" #パスワード
     BUTTON_LOGIN = "//button[@type='submit']" #ログイン
+
+class SignupPageLocators:
+    INPUT_EMAIL = "//input[@placeholder = 'メールアドレス']" #メールアドレス
+    INPUT_PASSWORD = "//input[@placeholder = 'パスワード']" #パスワード
+    INPUT_PASSWORD_CONFIRM = "//input[@placeholder = 'パスワード（確認）']" #パスワード（確認）
+    BUTTON_SIGNUP = "//button[@type='submit']" #サインアップ
