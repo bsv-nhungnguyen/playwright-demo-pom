9c918e0247ef59f833934ecbf41ecbb241b7901b
Nhung (VS Code) <nhungnguyen@bravesoft.com.vn>
2026-07-21 15:39:20 +0700

    fix feedback 001

--- locators.py
+++ locators.py
@@ -4,6 +4,9 @@ class LoginPageLocators:
     INPUT_PASSWORD = "//input[@placeholder = 'パスワード']" #パスワード
     BUTTON_LOGIN = "//button[@type='submit']" #ログイン
 
+class HomePageLocators:
+    BUTTON_LOGOUT = "//button[@class='logout-button']" #ログアウト
+    BUTTON_PROFILE = "//a[@href='/profile']" #プロフィール
     
 class SignupPageLocators:
     INPUT_EMAIL = "//input[@placeholder = 'メールアドレス']" #メールアドレス (Message)
